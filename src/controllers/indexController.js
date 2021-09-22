d3069c320c5ead4bcf78e4cf911b56e99dda4538
Jableed43 <javiernehuenlopez@gmail.com>
2021-09-22 16:13:41 -0300

    Upload archivos nuevos

---
+++ src/controllers/indexController.js
@@ -0,0 +1,16 @@
+const platos = require ('../data/platos.json')
+
+const controller = {
+    index: function(req, res, next) {
+        res.render('index', {platos});
+      },
+    detalle: (req, res) => {
+      const {id} = req.params
+      const plato = platos.find (element =>
+      element.id === +id)
+      // se agrega signo + dado que el id es numerico
+      res.render('detalleMenu', {plato})
+  } 
+}
+
+module.exports = controller;
